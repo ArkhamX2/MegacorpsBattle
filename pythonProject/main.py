e3baea66915c1ce6eaad6069c77569ac0eea9dfa
vadimyt <pvcs@yandex.ru>
2023-11-06 11:16:07 +0300

    test

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,19 +1,19 @@
 import pygame
 from hero import Player
-from client import  Client
+from client import Client
+ 
+pygame.init() # Инициализируем pygame
 
-pygame.init()  # Инициализируем pygame
+HOST, PORT = "localhost", 6666 # Адрес сервера
 
-HOST, PORT = "10.160.2.103", 8080  # Адрес сервера
-
-client = Client((HOST, PORT))  # Создаем объект клиента
-
-sсreen = pygame.display.set_mode((800, 600))  # Создаем окно с разрешением 800x600
-
-clock = pygame.time.Clock()  # Создаем объект для работы со временем внутри игры
+client = Client((HOST, PORT)) # Создаем объект клиента
+ 
+sсreen = pygame.display.set_mode((800, 600)) # Создаем окно с разрешением 800x600
+ 
+clock = pygame.time.Clock() # Создаем объект для работы со временем внутри игры
 
 while True:
-    for event in pygame.event.get():  # Перебираем все события которые произошли с программой
+    for event in pygame.event.get(): # Перебираем все события которые произошли с программой
         if event.type == pygame.KEYDOWN:
             if event.key == ord('a'):
                 client.move("left")
@@ -24,17 +24,17 @@ while True:
             if event.key == ord('s'):
                 client.move("down")
 
-        if event.type == pygame.QUIT:  # Проверяем на выход из игры
+        if event.type == pygame.QUIT: # Проверяем на выход из игры
             client.sock.close()
             exit()
 
-    sсreen.fill((0, 0, 0))  # Заполняем экран черным
+    sсreen.fill((0, 0, 0)) # Заполняем экран черным
 
     for i in client.players:
         print(i)
         player = Player((i["x"], i["y"]))
-        sсreen.blit(player.image, player.rect)  # Рисуем игрока
+        sсreen.blit(player.image, player.rect) # Рисуем игрока
 
-    pygame.display.update()  # Обновляем дисплей
-
-    clock.tick(60)  # Ограничиваем частоту кадров игры до 60адров игры до 60
+    pygame.display.update() # Обновляем дисплей
+ 
+    clock.tick(60) # Ограничиваем частоту кадров игры до 60
