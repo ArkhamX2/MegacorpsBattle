acd75431a5eb78767a60d003f54fc1a732857dc0
Timofey Latypov <timtompson3241@gmail.com>
2023-11-06 15:02:11 +0300

    WIP:client begins

--- pythonProject/library/cards/card.py
+++ pythonProject/library/cards/card.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import pygame
 
 class CardType(Enum):
     defence = 1
@@ -6,13 +7,20 @@ class CardType(Enum):
     developer = 3
 
 
-class Card:
+class Card(pygame.sprite.Sprite):
     cardName:str
     cardDescription:str
     price:int
     
 
-    def __init__(self,cardName:str, cardDescription:str):
+    def __init__(self,cardName:str, cardDescription:str, pos):        
+        pygame.sprite.Sprite.__init__(self)
+        # Загружаем спрайт игрока
+        self.image = pygame.image.load("resourses\\card.png").convert_alpha()
+        self.rect = self.image.get_rect(center=pos)
         self.cardName = cardName
         self.cardDescription = cardDescription
+
+
+
     
