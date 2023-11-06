1024ebdc04ce29332442d5789fbbded3781e2c2d
vadimyt <pvcs@yandex.ru>
2023-11-06 15:01:58 +0300

    new rework

--- test/server.py
+++ test/server.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
-import sys
+from player import Player
+import pickle
 
 class User():
     def __init__(self, id, ip):
@@ -8,7 +9,7 @@ class User():
         self.ip=ip
         self.connected=True
 
-server = "10.160.2.103"
+server = "localhost"
 port = 6666
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,23 +22,16 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
 
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100, 50,50, (0,0,255))]
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0),(100,100)]
-
-def threaded_client(conn, player,user):
-    conn.send(str.encode(make_pos(pos[player])))
+def threaded_client(conn, player, user):
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
             if not data:
                 user.connected=False
@@ -45,23 +39,23 @@ def threaded_client(conn, player,user):
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
-                #print("Received: ", data)
-                #print("Sending : ", reply)
+                print("Received: ", data)
+                print("Sending : ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
-
+    user.connected=False
     print("Lost connection")
-    user.connected = False
     conn.close()
+
+currentPlayer = 0
 dt = list()
 listusers=[]
-currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
