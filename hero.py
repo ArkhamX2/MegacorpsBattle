33a2f2acdcbcc9275ec434af147811128524dbfa
S1stem111 <150000187+S1stem111@users.noreply.github.com>
2023-11-06 11:14:51 +0300

    change

--- hero.py
+++ hero.py
@@ -8,6 +8,6 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         # Загружаем спрайт игрока
-        self.image = pygame.image.load("pythonProject/hero.png").convert_alpha()
+        self.image = pygame.image.load("hero.png").convert_alpha()
         # (400, 300) размеры экрана / 2, не стал делать в переменной
         self.rect = self.image.get_rect(center=pos)
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 from threading import Thread
 import json
 
-HOST, PORT = '10.160.2.103', 6666  # Адрес сервера
+HOST, PORT = '10.160.2.103', 8080  # Адрес сервера
 MAX_PLAYERS = 2  # Максимальное кол-во подключений
 
 
