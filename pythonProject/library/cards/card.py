5ae9d4de08ff1f91c966aa957e9269ccc0161469
Timofey Latypov <timtompson3241@gmail.com>
2023-11-06 15:43:17 +0300

    WIP: Cards flying!

--- pythonProject/library/cards/card.py
+++ pythonProject/library/cards/card.py
@@ -11,12 +11,16 @@ class Card(pygame.sprite.Sprite):
     cardName:str
     cardDescription:str
     price:int
+    isOposite:bool
     
 
-    def __init__(self,cardName:str, cardDescription:str, pos):        
+    def __init__(self,cardName:str, cardDescription:str, pos, isOposite:bool):        
         pygame.sprite.Sprite.__init__(self)
         # Загружаем спрайт игрока
-        self.image = pygame.image.load("resourses\\card.png").convert_alpha()
+        if isOposite:
+            self.image = pygame.image.load("pythonProject\\resourses\\cardOposite.png").convert_alpha()
+        else:
+            self.image = pygame.image.load("pythonProject\\resourses\\card.png").convert_alpha()
         self.rect = self.image.get_rect(center=pos)
         self.cardName = cardName
         self.cardDescription = cardDescription
