46f0486ad987b981d1f52f66bba8a7e51240731e
vadimyt <pvcs@yandex.ru>
2023-11-07 13:50:08 +0300

    collide detect

--- pythonProject/UI/handHandler.py
+++ pythonProject/UI/handHandler.py
@@ -2,13 +2,13 @@ import pygame
 
 class HandHandler():
 
-    def __init__(self,hand):
-        self.hand = hand
+    def __init__(self,handbox):
+        self.handbox = handbox
     
     def update(self,source):
-        self.hand.draw(source)
+        self.handbox.draw(source)
         
     def placeHand(self,source):
         winSize = pygame.display.get_window_size()
-        self.hand.x,self.hand.y = winSize[0]//2 - (self.hand.hand[0].ReturnToCard(self.hand.hand[0].isOposite).rect.width+self.hand.hand[0].ReturnToCard(self.hand.hand[0].isOposite).rect.width//16)*3,winSize[1]//2 +(self.hand.hand[0].ReturnToCard(self.hand.hand[0].isOposite).rect.height)//2 + 25
-        self.hand.draw(source)
+        self.handbox.x,self.handbox.y = winSize[0]//2 - (self.handbox.hand[0].ReturnToCard(self.handbox.hand[0].isOposite).rect.width+self.handbox.hand[0].ReturnToCard(self.handbox.hand[0].isOposite).rect.width//16)*3,winSize[1]//2 +(self.handbox.hand[0].ReturnToCard(self.handbox.hand[0].isOposite).rect.height)//2 + 25
+        self.handbox.draw(source)
