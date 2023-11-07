01fd4368f62c0435723c65a92d25d579391f6767
vadimyt <pvcs@yandex.ru>
2023-11-07 09:41:44 +0300

    board state messages

--- test/server.py
+++ test/server.py
@@ -22,52 +22,38 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
+d={"PHend": [[],[],[],[]], "BCards": [], "IsReady": [False, False, False, False]}
 
-players = [Player(0,0,50,50,(255,0,0)), Player(100,100, 50,50, (0,0,255)), Player(100,0,50,50,(0,255,0)), Player(0,100,50,50,(100,100,100))]
-
-def threaded_client(conn, player, user):
-    conn.send(pickle.dumps(players[player]))
+def threaded_client(conn, player):
+    conn.send(pickle.dumps([d["PHend"][player],d["BCards"],d["IsReady"]]))
     reply = ""
     while True:
         try:
             data = pickle.loads(conn.recv(2048))
-            players[player] = data
+
+            d["PHend"][player] = data[0]
+            d["BCards"]=data[1]
+            d["IsReady"]=data[2]
 
             if not data:
-                user.connected=False
                 print("Disconnected")
                 break
             else:
-                reply=[currentPlayer]
-                for i in range(len(players)):
-                    if (i!=player):
-                        reply.append(players[i])
+                reply=[d["PHend"][player],d["BCards"],d["IsReady"]]
 
-                print("Received: ", data)
-                print("Sending : ", reply)
+                #print("Received: ", data)
+                #print("Sending : ", reply)
 
             conn.sendall(pickle.dumps(reply))
         except:
             break
-    user.connected=False
     print("Lost connection")
     conn.close()
 
 currentPlayer = 0
-dt = list()
-listusers=[]
+
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-    if addr[0] in dt and False:
-        for user in listusers:
-            if user.connected != True and addr[0]==user.ip:
-                user.connected=True
-                start_new_thread(threaded_client, (conn, user.id,user))
-
-    else:
-        dt.append(addr[0])
-        print(dt)
-        listusers.append(User(currentPlayer, addr[0]))
-        start_new_thread(threaded_client, (conn, currentPlayer,listusers[len(listusers)-1]))
-        currentPlayer += 1
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
