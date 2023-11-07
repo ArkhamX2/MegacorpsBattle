76c23abe0a595623144a0698b895b029b9683663
vadimyt <pvcs@yandex.ru>
2023-11-07 11:30:55 +0300

    delete unnecessary and renaming

--- server/tester.py
+++ server/tester.py
@@ -1,23 +1,9 @@
 import pygame
 from network import Network
-from player import Player
 width = 500
 height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
-
-
-def redrawWindow(win,player, player2, player3, player4):
-    win.fill((255,255,255))
-    player.draw(win)
-    if player2!=None:
-        player2.draw(win)
-    if player3!=None:
-        player3.draw(win)
-    if player4!=None:
-        player4.draw(win)
-    pygame.display.update()
-
 
 def main():
     run = True
@@ -39,7 +25,6 @@ def main():
                 run = False
                 pygame.quit()
 
-        #p.move()
-        #redrawWindow(win, p, p2, p3, p4)
+        pygame.display.update()
 
 main()
